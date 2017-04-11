ca38a8a13f5dfdb14540a9c5cac97927bf2a5d01
Codymatthieu <cody.c.matthieu@gmail.com>
2017-04-11 14:31:35 -0700

    updated index

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 var ipfsAPI = require('ipfs-api');
-var ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'}); // leaving out the arguments will default to these values
+var ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'});
 const topic = 'general';
 
 const Blinkt = require('blinktjs');
 const blinkt = new Blinkt();
 
-  // https://github.com/ipfs/interface-ipfs-core/tree/master/API/pubsub#pubsubsubscribe
-  const receiveMsg = (msg) => {
-    console.log(msg.data.toString());
-    // if(isEven(msg.data.toString())){
-    if(msg.data.toString() == "on"){
-      //blinkt.setAll('white')
-//blinkt.setPixel(0, 255, 255, 255, 0.5);
-blinkt.setAll(0, 0, 255);
-blinkt.draw();
-setTimeout(() => {
-        console.log('end');
-}, 5000););
-    } else { }
+blinkt.setup();
+blinkt.clearAll();
+
+const receiveMsg = (msg) => {
+  console.log(msg.data.toString());
+
+  if(msg.data.toString() == "on") {
+    blinkt.setAllPixels(0, 0, 255, .5);
+    blinkt.sendUpdate();
+  }
+  else if (msg.data.toString() == "off") {
+    blinkt.setAllPixels(0, 0, 0, 0);
+    blinkt.sendUpdate();
+  }
+}
 
 ipfs.pubsub.subscribe(topic, receiveMsg);
-
-});
-
-
