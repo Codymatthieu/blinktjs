4c2a6690466f3e11324905bf9393bc737809a33f
codymatthieu <cody.c.matthieu@gmail.com>
2017-04-13 13:20:38 -0700

    should work....

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ var ipfsAPI = require('ipfs-api');
 var ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'});
 const topic = 'general';
 
-var Blinkt = require('blinktjs');
-var blinkt = new Blinkt();
-
-blinkt.setup();
-blinkt.clearAll();
+const Blinkt = require('blinktjs');
+const blinkt = new Blinkt();
 
 const receiveMsg = (msg) => {
-  console.log(msg.data.toString());
+	console.log(msg.data.toString());
 
-  if(msg.data.toString() == "on") {
-    blinkt.setAll(0, 0, 255);
-    blinkt.draw();
-  }
-  else if (msg.data.toString() == "off") {
-    blinkt.setAll(0, 0, 0);
-    blinkt.draw();
-  }
+	if(msg.data.toString() == "on") {
+		blinkt.setAll(0, 0, 255);
+		blinkt.draw();
+		console.log('blue');
+	}
+
+	else if (msg.data.toString() == "off") {
+		blinkt.setAll(0, 0, 0);
+		blinkt.draw();
+		console.log('off');
+	}
 }
 
 ipfs.pubsub.subscribe(topic, receiveMsg);
